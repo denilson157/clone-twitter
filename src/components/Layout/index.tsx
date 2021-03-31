0febafbc4def6e33ffbb18884891407aa36dd4fe
Denílson Pereira <dpereira@actioncargo.com.br>
2021-03-31 18:59:37 -0300

    informacoes do perfil

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from '../Main';
+
+import { Container, Wrapper } from './styles';
+
+const Layout: React.FC = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Main />
+                {/* <MenuBar />
+                <SideBar /> */}
+            </Wrapper>
+        </Container>
+    );
+}
+
+export default Layout;
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Container, Banner, Avatar, ProfileData, LocationIcon, CakeIcon, Followage } from './styles';
+
+const ProfilePage: React.FC = () => {
+    return (
+        <Container>
+            <Banner>
+                <Avatar />
+            </Banner>
+
+            <ProfileData>
+                {/* <EditButton outlined>
+                    Editar Perfil
+                </EditButton> */}
+
+                <h1>Denilson Pereira</h1>
+                <h2>@dpereira</h2>
+
+                <p>
+                    Full stack developer
+                </p>
+
+                <ul>
+                    <li>
+                        <LocationIcon />
+                        São Paulo, Brasil
+                    </li>
+                    <li>
+                        <CakeIcon />
+                        Nascido(a) em 27 de dezembro de 1998
+                    </li>
+                </ul>
+
+                <Followage>
+                    <span> Seguindo <strong>648</strong></span>
+                    <span> <strong>345</strong> seguidores </span>
+                </Followage>
+            </ProfileData>
+        </Container>
+    );
+}
+
+export default ProfilePage;
